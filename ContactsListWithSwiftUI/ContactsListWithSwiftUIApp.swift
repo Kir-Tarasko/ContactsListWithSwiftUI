dbff726ffc4bc4e627035969c9a5b11f36f2b31b
Кирилл Тараско <tarasko.kirill@icloud.com>
2022-03-03 19:33:40 +0200

    Initial Commit

---
+++ ContactsListWithSwiftUI/ContactsListWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsListWithSwiftUIApp.swift
+//  ContactsListWithSwiftUI
+//
+//  Created by Кирилл Тараско on 03.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsListWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
